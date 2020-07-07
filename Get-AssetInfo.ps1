382f0d243a5ad77026502a8abcf1db5ba6eaa0ca
tmurphyrrts <66630149+tmurphyrrts@users.noreply.github.com>
2020-07-07 10:36:52 -0500

    Update Get-AssetInfo.ps1
    Added additional metrics to upload

--- Get-AssetInfo.ps1
+++ Get-AssetInfo.ps1
@@ -3,40 +3,53 @@ Import-Module SnipeitPS
 $apikey = Get-Content -Path C:\Windows\Temp\snipeitapikey.txt
 $url = Get-Content -Path C:\Windows\Temp\snipeiturl.txt
 
-$serialNumber = (Get-WmiObject -Class win32_bios).SerialNumber
- 
-$assetExists = Get-Asset -search $serialNumber -url $url -apikey $apikey
-if(([string]::IsNullOrEmpty($assetExists)))
-{
-    $computerName = $env:COMPUTERNAME #Get computer name
-    $modelno = (Get-WmiObject -Class Win32_ComputerSystem).Model #Get machine model number
+$serialNumber = (Get-WmiObject -Class win32_bios).SerialNumber #Get machine serial number
+$computerName = $env:COMPUTERNAME #Get computer name
+$modelno = (Get-WmiObject -Class Win32_ComputerSystem).Model #Get machine model number
 
-    $cpu = ((Get-WmiObject -Class Win32_Processor).Name) -split "\sCPU.*" -replace "\(R\)|\(TM\)" #Get CPU name, remove (R) and (TM) and CPU speed from string
-    
-    $memory = (Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory #Get Memory amount
-    $memoryAmount = (([math]::Ceiling($memory / 1024 / 1024 / 1024).ToString()) + "GB") #Round up to nearest GB
-    
-    $storage = Get-Volume -DriveLetter C | Select-Object -ExpandProperty Size #Get Storage amount
-    $storageAmount = ([math]::Ceiling($storage / 1024 / 1024 / 1024)) #Round up to nearest GB
-    if (($storageAmount -gt 200) -and ($storageAmount -lt 260) ) {
-        $storageAmount = "256GB" #If total storage is between 200 and 260GB, set the amount to 256GB
-    } elseif (($storageAmount -gt 100) -and ($storageAmount -lt 130)) {
-        $storageAmount = "128GB" #If total storage is between 100 and 130GB, set the amount to 128GB
-    } elseif (($storageAmount -gt 470) -and ($storageAmount -lt 520)) {
-        $storageAmount = "512GB" #If total storage is between 470 and 520GB, set the amount to 512GB
-    }
+$loggedOnUser = Get-WMIObject -class Win32_ComputerSystem | Select-Object -ExpandProperty username #Get logged on user
 
-    $customFields = @{
-        "serial" = "$serialNumber"
-        "_snipeit_cpu_2" = "$cpu"
-        "_snipeit_memory_3" = "$memoryAmount"
-        "_snipeit_storage_4" = "$storageAmount"
-    }
+$OSVersion = (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ProductName).ProductName + " Version " + (Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion" -Name ReleaseID).ReleaseId #Get Operating system and version number
 
-    $modelSelection = Get-Model -url $url -apikey $apikey | Where-Object {$_.name -like "*$modelno*"}
- 
-    New-Asset -Name $computerName -tag $computerName -Model_id $modelSelection.id -Status "2" -customfields $customFields -url $url -apikey $apikey
+$internalIP = (Get-NetIPConfiguration | Where-Object {$_.IPv4DefaultGateway -ne $null -and $_.NetAdapter.Status -ne "Disconnected"}).IPv4Address.IPAddress #Get internal IP address
+$externalIP = (Invoke-RestMethod ipinfo.io/ip).Trim() #Get external IP address
+$MACAddress = (Get-NetAdapter | Where-Object {$_.Status -eq "Up"}).MacAddress #Get MAC address
+
+$cpu = ((Get-WmiObject -Class Win32_Processor).Name) -split "\sCPU.*" -replace "\(R\)|\(TM\)" #Get CPU name, remove (R) and (TM) and CPU speed from string
+$memory = (Get-WmiObject -Class Win32_ComputerSystem).TotalPhysicalMemory #Get Memory amount
+$memoryAmount = (([math]::Ceiling($memory / 1024 / 1024 / 1024).ToString()) + "GB") #Round up to nearest GB
+
+$storage = Get-Volume -DriveLetter C | Select-Object -ExpandProperty Size #Get Storage amount
+$storageAmount = ([math]::Ceiling($storage / 1024 / 1024 / 1024)) #Round up to nearest GB
+
+if (($storageAmount -gt 200) -and ($storageAmount -lt 260) ) {
+    $storageAmount = "256GB" #If total storage is between 200 and 260GB, set the amount to 256GB
+} elseif (($storageAmount -gt 100) -and ($storageAmount -lt 130)) {
+    $storageAmount = "128GB" #If total storage is between 100 and 130GB, set the amount to 128GB
+} elseif (($storageAmount -gt 470) -and ($storageAmount -lt 520)) {
+    $storageAmount = "512GB" #If total storage is between 470 and 520GB, set the amount to 512GB
 }
-else {
-    exit
+
+#Assign custom fields variables to values
+$customFields = @{
+    "serial"                            = "$serialNumber"
+    "_snipeit_cpu_2"                    = "$cpu"
+    "_snipeit_memory_3"                 = "$memoryAmount"
+    "_snipeit_storage_4"                = "$storageAmount"
+    "_snipeit_operating_system_6"       = "$OSVersion"
+    "_snipeit_last_logged_on_user_5"    = "$loggedOnUser"
+    "_snipeit_last_known_internal_ip_7" = "$internalIP"
+    "_snipeit_last_known_public_ip_8"   = "$externalIP"
+    "_snipeit_mac_address_9"            = "$MACAddress"
+}
+
+$modelID = (Get-Model -url $url -apikey $apikey | Where-Object {$_.name -like "*$modelno*"}).id #Get Snipe-IT Model ID based on model number pulled from machine
+
+$snipeAsset = Get-Asset -url $url -apikey $apikey -search $serialNumber #Search for asset in Snipe-IT based on S/N
+
+#If asset does not exist, create new asset
+if ([string]::IsNullOrEmpty($snipeAsset)) {
+    New-Asset -Name $computerName -Model_id $modelID -Status_id "2" -customfields $customFields -url $url -apikey $apikey #Create new Snipe-IT asset
+} else { #If asset already exists, update asset with new information
+    Set-Asset -id $snipeAsset.id -Name $computerName -model_id $modelID -status_id $snipeAsset.status_label.id -customfields $customFields -url $url -apikey $apikey #Update existing Snipe-IT asset
 }
